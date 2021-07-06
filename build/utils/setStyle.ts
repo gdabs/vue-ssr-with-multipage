e027cdb449fbb8c65965048dbeb1318003b48f44
guoding <guoding@xunlei.com>
2021-07-06 17:39:58 +0800

    perfect webpack

--- build/utils/setStyle.ts
+++ build/utils/setStyle.ts
@@ -1,6 +1,74 @@
+// import { join } from 'path';
 import * as WebpackChain from 'webpack-chain';
+// import { decl } from 'postcss';
 import { StyleOptions } from '@/interface/config';
 import { loadConfig } from './loadConfig';
+// import { getCwd } from './cwd';
+
+// const cwd = getCwd();
+
+// function wapSprites(rule, comment, image) {
+//   //百分比
+//   const backgroundSizes = image.spriteWidth / 100;
+//   let backgroundPositionX = (image.coords.x / (image.spriteWidth - image.coords.width)) * 100;
+//   let backgroundPositionY = (image.coords.y / (image.spriteHeight - image.coords.height)) * 100;
+
+//   backgroundPositionX = isNaN(backgroundPositionX) ? 0 : backgroundPositionX;
+//   backgroundPositionY = isNaN(backgroundPositionY) ? 0 : backgroundPositionY;
+
+//   function format(num: number) {
+//       return num < 10 ? '0' + num : `${num}`;
+//   };
+//   const date = new Date(),
+//       year = date.getFullYear(),
+//       month = format(date.getMonth() + 1),
+//       day = format(date.getDate()),
+//       hour = format(date.getHours()),
+//       currentTime = year + month + day + hour;
+
+//   const backgroundImage = decl({
+//       prop: 'background-image',
+//       value: 'url(' + image.spriteUrl + '?v=' + currentTime + ')'
+//   });
+
+//   const backgroundRepeat = decl({
+//       prop: 'background-repeat',
+//       value: 'no-repeat'
+//   });
+
+//   const backgroundSize = decl({
+//       prop: 'background-size',
+//       value: backgroundSizes + 'rem'
+//   });
+
+//   const backgroundPosition = decl({
+//       prop: 'background-position',
+//       value: backgroundPositionX + '% ' + backgroundPositionY + '%'
+//   });
+
+//   rule.insertAfter(comment, backgroundImage);
+//   rule.insertAfter(backgroundImage, backgroundPosition);
+//   rule.insertAfter(backgroundPosition, backgroundSize);
+//   rule.insertAfter(backgroundPosition, backgroundRepeat);
+//   // 百分比end
+// }
+
+
+// //雪碧图相关代码
+// // ----------合并雪碧图函数-----------
+// var spriteHooks = function () {
+//   return {
+//       onUpdateRule: wapSprites,
+//       onSaveSpritesheet: function (opts, spritesheet) {
+//           var url = Object.keys(spritesheet.coordinates)[0];
+//           var imageUrl = url.replace(/\\/g, '\/');
+//           var spritePath = imageUrl.slice(0, imageUrl.indexOf('/sprite'));
+//           var filenameChunks = spritesheet.groups.concat(spritesheet.extension);
+
+//           return join(spritePath, 'spr_' + filenameChunks.join('.'));
+//       }
+//   }
+// }
 
 const setStyle = (chain: WebpackChain, reg: RegExp, options: StyleOptions, isReact?: boolean) => {
   const { css, isDev } = loadConfig();
@@ -25,6 +93,44 @@ const setStyle = (chain: WebpackChain, reg: RegExp, options: StyleOptions, isRea
       ident: 'postcss',
       plugins: () =>
         [
+          require('postcss-opacity'),
+          require('postcss-assets')({
+            cachebuster: true,
+          }),
+          require('postcss-color-rgba-fallback')({
+            properties: ["background-color", "background", "color", "border", "border-color", "outline", "outline-color", "box-shadow"]
+          }),
+          require('postcss-px2rem-exclude')({
+            remUnit: 100,
+            exclude: /node_modules|folder_name/i
+          }),
+          // require('postcss-sprites')({
+          //   retina: false,//支持retina，可以实现合并不同比例图片
+          //   verbose: true,//Prints the plugin output to the console.
+          //   basePath: join(cwd, './static'),
+          //   spritePath: './', //雪碧图合并后存放地址，默认为./
+          //   stylesheetPath: null,//样式的位置，默认为null
+          //   spritesmith: {
+          //       padding: 8,
+          //       algorithm: 'binary-tree' //binary-tree, top-down, left-right, diagonal,alt-diagonal
+          //   },
+          //   filterBy: function (image) {
+          //       //过滤一些不需要合并的图片，返回值是一个promise，默认有一个exist的filter
+          //       if (image.url.indexOf('/sprite/') === -1) {
+          //           return Promise.reject();
+          //       }
+          //       return Promise.resolve();
+          //   },
+          //   groupBy: function (image) {
+          //       //将图片分组，可以实现按照文件夹生成雪碧图
+          //       var name = /\/sprite\/([0-9.A-Za-z\-\_]+)\//.exec(image.url);//取目录名称sprite和后面的目录名称
+          //       if (!name) {
+          //           return Promise.reject(new Error('Not a shape image.'));
+          //       }
+          //       return Promise.resolve(name[1]);
+          //   },
+          //   hooks: spriteHooks()
+          // }),
           require('postcss-flexbugs-fixes'),
           require('postcss-discard-comments'),
           require('postcss-preset-env')({
